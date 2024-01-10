10d4b6cf8fd71b125b2d058441d50b84125049e0
Tim <tim@tesouro.com>
2024-01-10 18:38:11 -0500

    middleware 4

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,17 @@
-import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+// import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
 import { NextResponse } from "next/server";
 
 import type { NextRequest } from "next/server";
-import type { Database } from "@/libs/database.types";
+// import type { Database } from "@/libs/database.types";
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
 
   // Create a Supabase client configured to use cookies
-  const supabase = createMiddlewareClient<Database>({ req, res });
+  // const supabase = createMiddlewareClient<Database>({ req, res });
 
-  // Refresh session if expired - required for Server Components
-  await supabase.auth.getSession();
+  // // Refresh session if expired - required for Server Components
+  // await supabase.auth.getSession();
 
   return res;
 }
