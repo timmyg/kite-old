61b18e9f2b17152954e53b8c11ae2f2fa927af01
Tim <tim@tesouro.com>
2024-01-11 12:11:27 -0500

    parse email

--- app/api/mail/inbound/route.ts
+++ app/api/mail/inbound/route.ts
@@ -1,8 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
+import sendgridParser from "@sendgrid/inbound-mail-parser";
 
 export async function POST(req: NextRequest) {
   const body = await new Response(req.body).text();
-  const body2 = await new Response(req.body).json();
-  console.info("Received an inbound email!", { body, body2 });
+  const parsedEmail = new sendgridParser(
+    { keys: ["from", "to", "subject", "text"] },
+    { body }
+  ).keyValues();
+  //   parsedEmail
+  console.info("Received an inbound email!", { parsedEmail });
   return NextResponse.json({ message: "excellent!" });
 }
