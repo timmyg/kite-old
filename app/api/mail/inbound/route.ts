3ef1c7e68364005fda94e700a1ef2a44bc092c7e
Tim <tim@tesouro.com>
2024-01-11 13:46:25 -0500

    .formdata

--- app/api/mail/inbound/route.ts
+++ app/api/mail/inbound/route.ts
@@ -1,6 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 import formidable from "formidable";
-import { NextApiRequest, NextApiResponse } from "next";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+// import { NextApiRequest, NextApiResponse } from "next";
 // import {
 //   parseEmail,
 //   storeAttachment,
@@ -9,15 +15,20 @@ import { NextApiRequest, NextApiResponse } from "next";
 export async function POST(req: NextRequest) {
   try {
     const form = new formidable.IncomingForm();
-    form.parse(req, (err: any, fields: any, files: any) => {
-      if (err) {
-        console.error(err);
-        return NextResponse.json({ message: "failde!" });
-      }
-      console.log({ fields, files });
-      return NextResponse.json({ message: "excellent!" });
-    });
-    // return NextResponse.json({ message: "excellent!" });
+    // const data = await req.formData();
+    let formData = await req.formData();
+    let body = Object.fromEntries(formData);
+    console.log({ body });
+    // form.parse(req, (err: any, fields: any, files: any) => {
+    //   if (err) {
+    //     console.error(err);
+    //     return NextResponse.json({ message: "failde!" });
+    //   }
+    //   console.log({ fields, files });
+    //   return NextResponse.json({ message: "excellent!" });
+    // });
+    // console.log()
+    return NextResponse.json({ message: "excellent!" });
   } catch (error) {
     console.error({ error });
     return NextResponse.json({ message: "failed" });
