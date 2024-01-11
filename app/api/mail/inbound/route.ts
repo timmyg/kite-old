3cd2fa68fa8d55b63496245e91673e8e59021a5a
Tim <tim@tesouro.com>
2024-01-11 15:07:58 -0500

    parsed 2

--- app/api/mail/inbound/route.ts
+++ app/api/mail/inbound/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import mailparser from "mailparser";
+import EmailReplyParser from "email-reply-parser";
 
 const simpleParser = mailparser.simpleParser;
 
@@ -30,8 +31,11 @@ export async function POST(req: Request) {
       envelope,
     });
 
-    let parsed = await simpleParser(email.toString());
-    console.log({ parsed });
+    let parsed1 = await simpleParser(email.toString());
+    console.log({ parsed1 });
+
+    const parsed2 = new EmailReplyParser().read(email.toString());
+    console.log({ parsed2 });
 
     // console.log({ requestBody });
     return NextResponse.json({ message: "excellent!" });
