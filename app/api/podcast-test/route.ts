40d34cbc69482a01ebe5a4df57607ac5ea6799cd
Tim <tim@tesouro.com>
2024-01-13 17:44:31 -0500

    another

--- app/api/podcast-test/route.ts
+++ app/api/podcast-test/route.ts
@@ -11,7 +11,7 @@ export async function GET() {
   console.log({ result123: result });
   if (!result.data || result.data.length === 0) {
     console.error("No emails found with a voice_text_url");
-    return new Response("No feed items found", { status: 404 });
+    return new Response("No feed items found 2", { status: 200 });
   }
   const feed = new RSS({
     title: "Sample RSS Feed 2",
