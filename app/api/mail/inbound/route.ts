5f88279a79ff27383f6148370085612f28a6d61c
Tim <tim@tesouro.com>
2024-01-22 18:49:22 -0500

    fixes

--- app/api/mail/inbound/route.ts
+++ app/api/mail/inbound/route.ts
@@ -2,7 +2,6 @@ import { NextResponse } from "next/server";
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import axios from "axios";
-import { convert as htmlToTextConcert } from "html-to-text";
 
 export async function POST(req: Request) {
   try {
@@ -44,7 +43,6 @@ export async function POST(req: Request) {
         from,
         subject,
         envelope,
-        body_text_2: htmlToTextConcert(String(email), {}),
       })
       .select();
     console.log("inserted ");
