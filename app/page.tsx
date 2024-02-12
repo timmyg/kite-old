785a48bd89a46c624d8a92bcb149fb40b38f04ca
Tim <tim@tesouro.com>
2024-02-12 16:05:58 -0500

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -10,10 +10,44 @@ import CTA from "@/components/CTA";
 import Footer from "@/components/Footer";
 // import FeaturesListicle from "@/components/FeaturesListicle";
 import Testimonials3 from "@/components/Testimonials3";
+import ButtonSignin from "@/components/ButtonSignin";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
+      {/* <main> */}
+      {/* <section className="flex flex-col items-center justify-center text-center gap-12 px-8 py-24">
+          <h1 className="text-3xl font-extrabold">Ship Fast ⚡️</h1>
+
+          <p className="text-lg opacity-80">
+            The start of your new startup... What are you gonna build?
+          </p>
+
+          <a
+            className="btn btn-primary"
+            href="https://shipfa.st/docs"
+            target="_blank"
+          >
+            Documentation & tutorials{" "}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="w-5 h-5"
+            >
+              <path
+                fillRule="evenodd"
+                d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </a>
+
+          <Link href="/blog" className="link link-hover text-sm">
+            Fancy a blog?
+          </Link>
+        </section> */}
       <Header />
       <main>
         <Hero />
@@ -24,6 +58,7 @@ export default function Home() {
         <Testimonials3 />
         <FAQ />
         <CTA />
+        {/* >>>>>>> ad7ff14 (hello world) */}
       </main>
       <Footer />
     </>
