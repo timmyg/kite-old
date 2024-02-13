f0a4fd9eb35afae25b6cda88d7b5d9da17f5fb96
Tim <tim@tesouro.com>
2024-02-13 07:04:10 -0500

    fix domain name

--- libs/util/server/url.ts
+++ libs/util/server/url.ts
@@ -13,7 +13,11 @@ export const getSiteConfig = (): SiteConfig => {
   const headersList = headers();
   const host = headersList.get("host");
   console.log({ host });
-  const domainName: string | null = host?.split(":")?.[0]?.split(".")?.[0];
+  // http://kite.localhost:3334 or https://www.kite.wtf => kite
+  const domainName: string | null = host
+    ?.replace(/^(?:https?:\/\/)?(?:www\.)?/, "")
+    ?.split(":")?.[0]
+    ?.split(".")?.[0];
   const site = sites.find((site) => site.domainName === domainName);
   if (!site) {
     throw new Error(`Site not found: ${{ domainName, host }}`);
